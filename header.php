d211223345e337c30f248f0da7ad056c1166ec1d
Jo Greybill <jogreybill@gmail.com>
2012-05-30 11:13:21 -0400

    Re-added the IE endif tag

--- header.php
+++ header.php
@@ -29,6 +29,7 @@
 		<?php endif;?>
 		<!--[if IE]>
 		<script type="text/javascript" src="<?=THEME_STATIC_URL?>/rgraph/excanvas/excanvas.compiled.js"></script>
+		<![endif]-->
 	</head>
 	<body class="<?=body_classes()?>">
 		<div class="container">
